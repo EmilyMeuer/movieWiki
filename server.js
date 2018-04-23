be7325fff14b7902f54963a9ed2b3ceb4043b0bd
vincentlee9512 <li000053@stthomas.edu>
2018-04-22 22:44:55 -0500

    able to display results

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //This is built-in module
 var express = require('express');
 var fs = require('fs');
+var querystring = require('querystring');
 var path = require('path');
 var http = require('http');
 var url = require('url');
@@ -10,8 +11,6 @@ var multiparty = require('multiparty');
 var mime = require('mime');
 var sqlite3 = require('sqlite3').verbose();
 
-
-var db = new sqlite3.Database('../imdb.sqlite3');
 var app = express();
 var port = 8011;
 var public_dir = path.join(__dirname, 'public');
@@ -60,18 +59,85 @@ app.use(express.static(public_dir));
 //users should not do it this way
 //give error
 app.get('/search', (req, res) => {
+    var req_url = url.parse(req.url);
+    var query = querystring.parse(req_url.query);
+
+    console.log(query);
+
+    res.writeHead(404, {'Content-Type' : 'text/plain'});
     var resStr = 'Wrong action, please go back to home page.';
     resStr += '<br/><a href="/index.html">home page</a>';
-    res.send(resStr);
+    res.write(resStr);
+    res.end();
 });
 
 //testing the POST method with /search
 app.post('/search', (req, res) => {
-   res.sendFile(public_dir + '/results-template.html');
+    var form = new multiparty.Form();
+    form.parse(req, (err, fields, files) => {
+
+        if(err){
+            res.writeHead(404, {'Content-Type' : 'text/plain'});
+            var resStr = 'something wrong here, please go back to home page.';
+            resStr += '<br/><a href="/index.html">home page</a>';
+            res.write(resStr);
+            res.end();
+        }else{
+
+            var fieldsStr = "Type: ";
+            fieldsStr += fields.type[0];
+            fieldsStr += "<br/>" + "Content: ";
+            fieldsStr += fields.content[0];
+
+            var sql = "SELECT * FROM Titles WHERE primary_title LIKE '%" + fields.content[0] +"%'";
+            console.log(sql);
+
+            fs.readFile('public/results-template.html', (err, data) => {
+                if(err){
+                    console.log(err);
+                    res.writeHead(404, {'Content-Type': 'text/plain'});
+                    res.write('Uh oh - could not find file. here');
+                    res.end();
+                }else{
+                    var db = new sqlite3.Database('../imdb.sqlite3');
+
+                    db.all(sql, function (err, rows) {
+                        if(err){
+                            console.log(err);
+                        }else{
+                            var table_html_code = "<div class='list-group'>";
+
+                            for(var i=0; i<rows.length; i++){
+                                table_html_code += "<a href=\"#\" class=\"list-group-item list-group-item-action \">";
+                                table_html_code += rows[i].primary_title;
+                                table_html_code += "</a>";
+                            }
+
+                            table_html_code += "</div>";
+                            //console.log(table_html_code);
+
+                            res.writeHead(200, {'Content' : 'text/html'});
+                            var html_code = data.toString('utf8');
+                            html_code = html_code.replace('***TABLE***', table_html_code);
+                            res.write(html_code);
+                            res.end();
+
+                            db.close();
+                        }
+                    });
+
+
+
+                }
+            });
+        }
+    });
+
+
 });
 
 
-var sql = "SELECT * FROM Titles WHERE primary_title LIKE '%spiderman%'; ";
+//var sql = "SELECT * FROM Titles WHERE primary_title LIKE '%spiderman%'; ";
 
 var hello = function(err, rows){
     if(err){
@@ -87,5 +153,3 @@ var hello = function(err, rows){
 console.log("now listening at port: " + port);
 
 app.listen(port, '0.0.0.0');
-
-db.close();
